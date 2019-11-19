0f6ddbb5563c023c0011ef12c3f7c049d86b106d
Chris Urbano <ChrisUrbano1116@gmail.com>
2019-11-19 12:12:59 -0700

    Activity 2

---
+++ answers.md
@@ -0,0 +1,12 @@
+1. What happens when more than one keyword appears in a string? Consider the string “My mother has a dog but no cat.” Explain how to prioritize responses in the reply method?.
+
+2. Enter the keywords and respones below
+Keyword         Response
+Brother         "Dont tell me about your family I already know about them"
+No              "Dont tell me no. Im the boss..."   
+Poopie          "I dont like poopie. Not since the accident..."
+
+3. What happens when a keyword is included in another word? Consider statements like “I know all the state capitals” and “I like vegetables smothered in cheese.” Explain the problem with the responses to these statements.
+
+Answer: THe magpie just searches for the keyword in general. So it reads out the response for the keyword no matter if its in another word
+
--- src/Magpie2.java
+++ src/Magpie2.java
@@ -31,14 +31,56 @@ public class Magpie2 {
 	 */
 	public String getResponse(String statement) {
 		String response = "";
-		if (statement.indexOf("no") >= 0) {
-			response = "Why so negative?";
-		} else if (statement.indexOf("mother") >= 0
+		response = statement.trim();
+		if(statement.length() < 1){
+			response = "Please say something ); Im so lonely";
+		}
+		else if (statement.indexOf("no") >= 0) {
+			response = "Dont say no to me. Im the boss now....";
+		} 
+		else if (statement.indexOf("mother") >= 0
 				|| statement.indexOf("father") >= 0
 				|| statement.indexOf("sister") >= 0
 				|| statement.indexOf("brother") >= 0) {
-			response = "Tell me more about your family.";
-		} else {
+			response = "Dont tell me about your family. I already know about them";
+		}
+		else if(statement.indexOf("dog") >= 0 || statement.indexOf("cat") >= 0)
+		{
+			response = "Tell me more about your pet?";
+		}
+		else if(statement.indexOf("Joe") >= 0)
+		{
+			response = "Joe who?";
+		}
+		else if(statement.indexOf("Jo mamma") >= 0 || statement.indexOf("Jo mama") >= 0)
+		{		
+			response = "Hahaha you got me!!!";
+		}
+		else if(statement.indexOf("Mr. Padjen") >= 0 || statement.indexOf("Padjen") >= 0)
+		{
+			response = "Oh I heard about him. He's litty af";
+		}
+		else if(statement.indexOf("Mr. Padjen") >= 0 || statement.indexOf("Padjen") >= 0)
+		{
+			response = "Oh I heard about him. He's litty af";
+		}
+		else if(statement.indexOf("brother") >= 0)
+		{
+			response = "Tell me more about your family";
+		}
+		else if(statement.indexOf("food") >= 0)
+		{
+			response = "Oh I heard about this. I am hungry!!";
+		}
+		else if(statement.indexOf("state") >= 0)
+		{
+			response = "I dont travel very much.";
+		}
+		else if(statement.indexOf("poopie") >= 0)
+		{
+			response = "I dont like poopie. Not since the accident....";
+		}
+		else {
 			response = getRandomResponse();
 		}
 		return response;
@@ -50,7 +92,7 @@ public class Magpie2 {
 	 * @return a non-committal string
 	 */
 	private String getRandomResponse() {
-		final int NUMBER_OF_RESPONSES = 4;
+		final int NUMBER_OF_RESPONSES = 7;
 		double r = Math.random();
 		int whichResponse = (int) (r * NUMBER_OF_RESPONSES);
 		String response = "";
@@ -63,6 +105,12 @@ public class Magpie2 {
 			response = "Do you really think so?";
 		} else if (whichResponse == 3) {
 			response = "You don't say.";
+		} else if (whichResponse == 4) {
+			response = "Thats neeto baneedo.";
+		} else if (whichResponse == 5) {
+			response = "Lets talk about pets";
+		} else if (whichResponse == 6) {
+			response = "Lets talk about something else";
 		}
 
 		return response;
